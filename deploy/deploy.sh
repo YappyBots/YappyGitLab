c0ad82efe01d0973862a4eb81fea6b8725cae53c
David Sevilla Martin <dsevilla192@icloud.com>
2017-04-29 17:56:08 -0400

    ci(travis): fix deploy.sh... lol that was for site

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,13 +3,48 @@
 
 set -e
 
-if [ "$TRAVIS_BRANCH" != "stable" -o -n "$TRAVIS_TAG" -o "$TRAVIS_PULL_REQUEST" != "false" ]; then
-  echo -e "\e[36m\e[1mNot building for a stable branch push - building without deploying."
-  npm run build
+function tests {
+  npm run lint
+  npm run docs:test
+  VERSIONED=false npm run webpack
+  exit 0
+}
+
+function build {
+  npm run lint
+  npm run docs
+  VERSIONED=false npm run webpack
+}
+
+# For revert branches, do nothing
+if [[ "$TRAVIS_BRANCH" == revert-* ]]; then
+  echo -e "\e[36m\e[1mBuild triggered for reversion branch \"${TRAVIS_BRANCH}\" - doing nothing."
   exit 0
 fi
 
-echo -e "\e[36m\e[1mBuilding for a stable branch push - building and deploying."
+# For PRs, only run tests
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+  echo -e "\e[36m\e[1mBuild triggered for PR #${TRAVIS_PULL_REQUEST} to branch \"${TRAVIS_BRANCH}\" - only running tests."
+  tests
+fi
+
+if [ -n "$TRAVIS_TAG" ]; then
+  echo -e "\e[36m\e[1mBuild triggered for tag \"${TRAVIS_TAG}\"."
+  SOURCE=$TRAVIS_TAG
+  SOURCE_TYPE="tag"
+else
+  echo -e "\e[36m\e[1mBuild triggered for branch \"${TRAVIS_BRANCH}\"."
+  SOURCE=$TRAVIS_BRANCH
+  SOURCE_TYPE="branch"
+fi
+
+# For Node != 6, only run tests
+if [ "$TRAVIS_NODE_VERSION" != "6" ]; then
+  echo -e "\e[36m\e[1mBuild triggered with Node v${TRAVIS_NODE_VERSION} - only running tests."
+  tests
+fi
+
+build
 
 # Initialise some useful variables
 REPO=`git config remote.origin.url`
@@ -17,16 +52,20 @@ SSH_REPO="https://datitisev:$ENCRYPTED_TOKEN@github.com/datitisev/DiscordBot-Yap
 SHA=`git rev-parse --verify HEAD`
 
 # Checkout the repo in the target branch
-TARGET_BRANCH="gh-pages"
-git clone $REPO dist -b $TARGET_BRANCH
+TARGET_BRANCH="docs"
+git clone $REPO out -b $TARGET_BRANCH
 
-# Build the site
-npm run build
+# Move the generated JSON file to the newly-checked-out repo, to be committed and pushed
+mv docs/docs.json out/$SOURCE.json
 
 # Commit and push
-cd dist
-git add --all .
+cd out
+git add .
 git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
-git commit -m "Site build: ${SHA}" || true
+git commit -m "Docs build for ${SOURCE_TYPE} ${SOURCE}: ${SHA}" || true
 git push -q $SSH_REPO $TARGET_BRANCH &> /dev/null
+
+# Clean up...
+cd ..
+rm -rf out
